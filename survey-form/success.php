be3a89799c5a3e29f590bb0aca982fdea44f09cc
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2021-08-03 23:54:25 +0800

    Work in progress

---
+++ survey-form/success.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Corben&family=Montserrat:wght@500&family=Mukta:wght@500&family=Roboto&display=swap" rel="stylesheet">
+
+</head>
+
+
+<body>
+
+    <div class = 'parallax'>
+        <div id = 'heading'>
+            <h1 id = 'title'> Thank you, <?php echo $_GET['sex']?>.</h1>
+            <p id = 'description'>
+            Rest assure that we will do everything we can to improve the hotel.
+            </p>
+            <div id = 'link'>
+                <a href="index.html">Go back to Form</a>
+            </div>
+        </div>
+    </div>
+    
+</body>
+
+
+</html>
